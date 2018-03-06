f61d26be8fd6835f8881dffa2815bc853d36b471
Josh Winter <winterjoshr@gmail.com>
2018-03-06 02:49:53 -0500

    use variable to determin qt version in winbuild.bat

--- winbuild.bat
+++ winbuild.bat
@@ -1,6 +1,8 @@
+set qtver=5.9.1
+
 rd /s /q winqt
 mkdir winqt
 
-cl /I"C:\Qt\5.10.0\msvc2017_64\include" /I"C:\Qt\5.10.0\msvc2017_64\include\QtCore" /I"C:\Qt\5.10.0\msvc2017_64\include\QtGui" /I"C:\Qt\5.10.0\msvc2017_64\include\QtWidgets" /EHsc *.cpp ws2_32.lib C:\Qt\5.10.0\msvc2017_64\lib\Qt5Core.lib C:\Qt\5.10.0\msvc2017_64\lib\Qt5Widgets.lib C:\Qt\5.10.0\msvc2017_64\lib\Qt5Gui.lib /link /out:winqt\stbsrisrates.exe
+cl /I"C:\Qt\%qtver%\msvc2017_64\include" /I"C:\Qt\%qtver%\msvc2017_64\include\QtCore" /I"C:\Qt\%qtver%\msvc2017_64\include\QtGui" /I"C:\Qt\%qtver%\msvc2017_64\include\QtWidgets" /EHsc *.cpp ws2_32.lib C:\Qt\%qtver%\msvc2017_64\lib\Qt5Core.lib C:\Qt\%qtver%\msvc2017_64\lib\Qt5Widgets.lib C:\Qt\%qtver%\msvc2017_64\lib\Qt5Gui.lib /link /out:winqt\stbsrisrates.exe
 
-c:\Qt\5.10.0\msvc2017_64\bin\windeployqt.exe --release winqt\stbsrisrates.exe
+c:\Qt\%qtver%\msvc2017_64\bin\windeployqt.exe --release winqt\stbsrisrates.exe
