d8c1a96c7f44c3ba59bf150ecef93a60cd1569b1
Josh Winter <winterjoshr@gmail.com>
2018-03-06 23:23:32 -0500

    fix winbuild asset copies again again again again again sheesh

--- winbuild.bat
+++ winbuild.bat
@@ -2,6 +2,7 @@ call qtver.bat
 
 rd /s /q winqt
 mkdir winqt
+xcopy assets winqt\assets /E /h /I
 
 cl /I"C:\Qt\%qtver%\msvc2017_64\include" /I"C:\Qt\%qtver%\msvc2017_64\include\QtCore" /I"C:\Qt\%qtver%\msvc2017_64\include\QtGui" /I"C:\Qt\%qtver%\msvc2017_64\include\QtWidgets" /EHsc *.cpp ws2_32.lib C:\Qt\%qtver%\msvc2017_64\lib\Qt5Core.lib C:\Qt\%qtver%\msvc2017_64\lib\Qt5Widgets.lib C:\Qt\%qtver%\msvc2017_64\lib\Qt5Gui.lib /link /out:winqt\stbsrisrates.exe
 
