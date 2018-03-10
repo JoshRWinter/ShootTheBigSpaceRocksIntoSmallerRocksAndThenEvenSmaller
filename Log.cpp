a6c90f3fbb931557b1214e70e998324d8189379f
Josh Winter <winterjoshr@gmail.com>
2018-03-10 13:59:34 -0500

    clean up log(...) macro

--- Log.cpp
+++ Log.cpp
@@ -6,18 +6,15 @@
 
 #include "Log.h"
 
-void log_stdout(const char *prefix, const char *fmt, ...)
+void log_stdout(const char *fmt, ...)
 {
 	static std::mutex stdout_lock;
 	std::lock_guard<std::mutex> lock(stdout_lock);
-
-	printf("%s", prefix);
 
 	va_list list;
 	va_start(list, fmt);
 	vprintf(fmt, list);
 	va_end(list);
 
-	puts("");
 	fflush(stdout);
 }
--- Log.h
+++ Log.h
@@ -1,7 +1,7 @@
 #ifndef LOG_H
 #define LOG_H
 
-void log_stdout(const char*, const char*, ...);
-#define log(fmt, ...) log_stdout((std::string(__FILE__) + " (" + __func__ + "():" + std::to_string(__LINE__) + "): ").c_str(), fmt, ##__VA_ARGS__)
+void log_stdout(const char*, ...);
+#define log(fmt, ...) log_stdout("%s:%s():%d: " fmt "\n", __FILE__, __func__, __LINE__, ##__VA_ARGS__)
 
 #endif // LOG_H
