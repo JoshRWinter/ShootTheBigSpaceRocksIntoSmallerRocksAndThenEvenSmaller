53fcf26617f2f29d7b2b78dfba945e4661c3cd47
Josh Winter <winterjoshr@gmail.com>
2018-03-17 14:32:12 -0400

    windows: change build script to reference %qtpath% variable

--- README.md
+++ README.md
@@ -15,5 +15,5 @@ video: https://streamable.com/0z644
 ## WINDOWS
 1. Install MSVC++
 2. open "Native Tools command prompt (x64)"
-3. `echo 'set qtver=5.10.0' > qtver.bat` (for appropriate qt version)
+3. `echo set qtpath=C:\Path\To\Qt\5.10.0\msvc2017_64>qtpath.bat` <-- don't put spaces around the `<` in `...>qtpath.bat`
 4. `.\winbuild.bat`
--- winbuild.bat
+++ winbuild.bat
@@ -1,11 +1,11 @@
-call qtver.bat
+call qtpath.bat
 
 rd /s /q winqt
 mkdir winqt
 xcopy assets winqt\assets /E /h /I
 
-cl /I"C:\Qt\%qtver%\msvc2017_64\include" /I"C:\Qt\%qtver%\msvc2017_64\include\QtCore" /I"C:\Qt\%qtver%\msvc2017_64\include\QtGui" /I"C:\Qt\%qtver%\msvc2017_64\include\QtWidgets" /I"C:\Qt\%qtver%\msvc2017_64\include\QtGamepad" /EHsc *.cpp ws2_32.lib C:\Qt\%qtver%\msvc2017_64\lib\Qt5Core.lib C:\Qt\%qtver%\msvc2017_64\lib\Qt5Widgets.lib C:\Qt\%qtver%\msvc2017_64\lib\Qt5Gui.lib c:\Qt\%qtver%\msvc2017_64\lib\Qt5Gamepad.lib /link /out:winqt\stbsrisrates.exe
+cl /I%qtpath%\include /I%qtpath%\include\QtCore /I%qtpath%\include\QtGui /I%qtpath%\include\QtWidgets /I%qtpath%\include\QtGamepad /EHsc *.cpp ws2_32.lib %qtpath%\lib\Qt5Core.lib %qtpath%\lib\Qt5Widgets.lib %qtpath%\lib\Qt5Gui.lib %qtpath%\lib\Qt5Gamepad.lib /link /out:winqt\stbsrisrates.exe
 
 cl /EHsc /DFREE_SERVER Server.cpp GameState.cpp Log.cpp network.cpp ws2_32.lib /link /out:winqt/stbsrisrates-dedicated.exe
 
-c:\Qt\%qtver%\msvc2017_64\bin\windeployqt.exe --release winqt\stbsrisrates.exe
+%qtpath%\bin\windeployqt.exe --release winqt\stbsrisrates.exe
